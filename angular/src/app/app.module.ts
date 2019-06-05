788b33b1ac793b1cf1c0ee64adf8eb3fef6bbbb1
9plus <541004828@qq.com>
2019-06-06 00:29:10 +0800

    add background.js

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HomeComponent } from './home/home.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+/// <reference types="chrome"/>
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,13 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // the block ads flag
+  isBlockAds: boolean;
 
-  check: boolean = false;
-  menu: string[] = ['Avoid Ads', 'Info', 'Sign Out'];
+  // background.js
+  bg: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.bg = chrome.extension.getBackgroundPage();
+
+    // every time we open the popup, we need read the settings saved in background.js
+    this.isBlockAds = this.bg.isBlockAds;
   }
 
+  /**
+   * If trigger toggleChange or dragChange event, we will
+   * call this function.
+   */
+  onSlideChange() {
+    this.isBlockAds = !this.isBlockAds; // change the flag value
+    this.bg.isBlockAds = this.isBlockAds; // save the value in background.js
+  }
 }
