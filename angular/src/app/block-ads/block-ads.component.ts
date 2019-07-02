69b330624525a001ec7bb7b21880c19cdd6cc443
9plus <541004828@qq.com>
2019-07-02 21:57:52 +0800

    更新配置界面

--- angular/src/app/block-ads/block-ads.component.ts
+++ angular/src/app/block-ads/block-ads.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {StorageManager} from "../common/storage.manager";
 
 @Component({
   selector: 'app-block-ads',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockAdsComponent implements OnInit {
 
-  constructor() { }
+  isHideFansCard: boolean;
+
+  constructor(private ngZone: NgZone) { }
 
   ngOnInit() {
+    const storageKeys = [StorageManager.HIDE_FANS_CARD_KEY];
+    chrome.storage.sync.get(storageKeys, items => {
+      // Because the data-bind won't update the view in time. We need ngZone to update the view.
+      this.ngZone.run(() => {
+        this.isHideFansCard = items.isHideFansCard;
+      });
+    });
   }
 
+  onHideFansCardClick() {
+    this.isHideFansCard = !this.isHideFansCard;
+    chrome.storage.sync.set({isHideFansCard: this.isHideFansCard});
+  }
 }
--- angular/src/app/common/storage.manager.ts
+++ angular/src/app/common/storage.manager.ts
@@ -1,4 +1,5 @@
 export class StorageManager {
   static BLOCK_ADS_KEY = 'isBlockAds';
   static CATCH_BARRAGE_KEY = 'isCatchBarrage';
+  static HIDE_FANS_CARD_KEY = 'isHideFansCard'
 }
